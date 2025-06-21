169444f68ad90464418661e0d4822746c698dfe0
Light2DAnimaNera <141644942+Light2DAnimaNera@users.noreply.github.com>
2025-06-21 13:10:06 +0300

    Remove reply keyboard

--- bot_commands.py
+++ bot_commands.py
@@ -11,11 +11,3 @@ def setup_default_commands(bot: telebot.TeleBot) -> None:
     bot.set_my_commands(
         [telebot.types.BotCommand(cmd, desc) for cmd, desc in DEFAULT_COMMANDS]
     )
-
-
-def build_commands_keyboard() -> telebot.types.ReplyKeyboardMarkup:
-    """Builds a keyboard with available bot commands."""
-    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    for cmd, _ in DEFAULT_COMMANDS:
-        keyboard.add(f"/{cmd}")
-    return keyboard
