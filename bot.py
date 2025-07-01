d75e69d39409f9284a336a7c8db7e49e4444b995
Light2DAnimaNera <141644942+Light2DAnimaNera@users.noreply.github.com>
2025-07-01 12:05:11 -0500

    Add idle session termination

--- bot.py
+++ bot.py
@@ -1,16 +1,26 @@
 import telebot
 import signal
+import threading
+import time
 
 from env import TELEGRAM_TOKEN
 from handlers import register_handlers
 from bot_commands import setup_default_commands
 from session_manager import SessionManager
 from middlewares_error import ErrorMiddleware
+from middlewares_activity import ActivityMiddleware
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN, use_class_middlewares=True)
 bot.setup_middleware(ErrorMiddleware())
+bot.setup_middleware(ActivityMiddleware())
 setup_default_commands(bot)
 register_handlers(bot)
+
+
+def _session_monitor() -> None:
+    while True:
+        SessionManager.expire_idle(bot, 60)
+        time.sleep(5)
 
 def _stop_bot(*_: object) -> None:
     """Gracefully stop polling and worker pool."""
@@ -20,6 +30,7 @@ def _stop_bot(*_: object) -> None:
 def main() -> None:
     signal.signal(signal.SIGINT, _stop_bot)
     signal.signal(signal.SIGTERM, _stop_bot)
+    threading.Thread(target=_session_monitor, daemon=True).start()
     try:
         bot.infinity_polling(logger_level=None)
     except KeyboardInterrupt:
---
+++ middlewares_activity.py
@@ -0,0 +1,18 @@
+from telebot.handler_backends import BaseMiddleware
+from telebot.types import Message
+
+from session_manager import SessionManager
+
+
+class ActivityMiddleware(BaseMiddleware):
+    def __init__(self) -> None:
+        self.update_sensitive = False
+        self.update_types = ['message']
+
+    def pre_process(self, message: Message, data):
+        if SessionManager.active(message.from_user.id):
+            SessionManager.update_activity(message.from_user.id)
+
+    def post_process(self, message: Message, data, exception):
+        pass
+
