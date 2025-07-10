c791946c0498685e9559ede4c9b3a4ae15e422bf
Light2DAnimaNera <141644942+Light2DAnimaNera@users.noreply.github.com>
2025-07-10 15:46:55 +0200

    feat(dss): send replies via ds bot

--- bots/DSS/bot.py
+++ bots/DSS/bot.py
@@ -1,7 +1,8 @@
 import telebot
-from shared.env import TELEGRAM_TOKEN_BOT3
+from shared.env import TELEGRAM_TOKEN_BOT3, TELEGRAM_TOKEN_BOT1
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN_BOT3)
+ds_bot = telebot.TeleBot(TELEGRAM_TOKEN_BOT1)
 
 from .handlers import register_handlers
 register_handlers(bot)
